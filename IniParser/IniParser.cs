66bdc183705a421232e62915ee60f530b5922c64
Neli <51109104+Neliitsme@users.noreply.github.com>
2020-10-11 14:44:51 +0300

    Add files via upload

--- IniParser/IniParser.cs
+++ IniParser/IniParser.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -10,93 +11,51 @@ using Microsoft.VisualBasic;
 
 namespace IniParser
 {
-    // class IniCollection
-    // {
-    //     public Dictionary<string, Dictionary<string, string>> iniCollection = new Dictionary<string, Dictionary<string, string>>();
-    // }
-    
-    
-    
     class IniParser
     {
         private string _filePath;
-        private string _sectionPattern = @"\[(.*?)\]";
-        private Dictionary<string, Dictionary<string, string>> _iniCollection = new Dictionary<string, Dictionary<string, string>>();
-        // public IniCollection data = new IniCollection();
+        private readonly string _sectionPattern = @"\[(.*?)\]";
 
-        private string _WhtiespaceRemover(string line)
+        private readonly Dictionary<string, Dictionary<string, string>> _iniCollection =
+            new Dictionary<string, Dictionary<string, string>>();
+
+        private static string _WhitespaceRemover(string line)
         {
             line = string.Join("", line.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries));
             return line;
         }
-        
-        private string _CommentRemover(string line)
+
+        private static string _CommentRemover(string line)
         {
             if (line.Contains(";"))
             {
                 line = line.Remove(line.IndexOf(";", StringComparison.Ordinal));
             }
-            
+
             return line;
         }
 
-        private bool _CheckIfSectionOrParameterExists(string sectionName, string parameterName)
-        {
-            if (!_iniCollection.ContainsKey(sectionName))
-            {
-                return false;
-            }
+        private bool _CheckIfSectionOrParameterExists(string sectionName, string parameterName) =>
+            _iniCollection.ContainsKey(sectionName) && _iniCollection[sectionName].ContainsKey(parameterName);
 
-            if (!_iniCollection[sectionName].ContainsKey(parameterName))
-            {
-                return false;
-            }
-            
-            return true;
-        }
-        
-        public void TryGetInt(string sectionName, string parameterName, out int value)
+        public void TryGet<T>(string sectionName, string parameterName, out T value)
         {
             if (!_CheckIfSectionOrParameterExists(sectionName, parameterName))
             {
                 throw new ArgumentException("Incorrect Section or Parameter name");
             }
 
-            var outcome = int.TryParse(_iniCollection[sectionName][parameterName], out var returnVal);
-            if (outcome)
+            try
             {
-                value = returnVal;
+                value = (T) Convert.ChangeType(_iniCollection[sectionName][parameterName], typeof(T));
             }
-            else
+            catch (FormatException)
             {
-                throw new FormatException("Cannot convert this string to int");
-            }
-        }
-
-        public void TryGetFloat(string sectionName, string parameterName, out float value)
-        {
-            if (!_CheckIfSectionOrParameterExists(sectionName, parameterName))
-            {
-                throw new ArgumentException("Incorrect Section or Parameter name");
-            }
-            
-            var outcome = float.TryParse(_iniCollection[sectionName][parameterName], out var returnVal);
-            if (outcome)
-            {
-                value = returnVal;
-            }
-            else
-            {
-                throw new FormatException("Cannot convert this string to float");
+                throw new FormatException($"Cannot convert this string to {typeof(T)}");
             }
         }
 
-        public void TryGetString(string sectionName, string parameterName, out string value)
-        {
-            value = _iniCollection[sectionName][parameterName];
-        }
-        
-        public int SetFilePath(string path, bool debug)
+        public void SetFilePath(string path, bool debug)
         {
             if (!File.Exists(path))
             {
@@ -115,8 +74,6 @@ namespace IniParser
                     Console.WriteLine("File path's set");
                     break;
             }
-
-            return 0; // All good, path is set
         }
         public void PrintIniCollection()
         {
@@ -131,34 +88,39 @@ namespace IniParser
         }
         public Dictionary<string, Dictionary<string, string>> IniParse()
         {
-            string[] allLines = File.ReadAllLines(_filePath);
-            Regex rgxSectionPattern = new Regex(_sectionPattern);
-            string lastSection = "NO_SECTION";
+            var allLines = File.ReadAllLines(_filePath);
+            var rgxSectionPattern = new Regex(_sectionPattern);
+            string lastSection = "NO_SECTION_ENTRY";
             int counter = 1;
 
+            if (!rgxSectionPattern.IsMatch(allLines[0]))
+            {
+                _iniCollection.Add(lastSection, new Dictionary<string, string>());
+            }
+            
             foreach (string s in allLines)
             { 
                 var commentlessString = _CommentRemover(s);
                 
-                if (String.IsNullOrWhiteSpace(commentlessString))
+                if (string.IsNullOrWhiteSpace(commentlessString))
                 {
-                    counter += 1;
+                    counter++;
                     continue;
                 }
                 if (rgxSectionPattern.IsMatch(commentlessString))
                 {
-                    string trimmedSectionName = s.Trim(new Char[] {'[', ']', ' '});
+                    string trimmedSectionName = s.Trim('[', ']', ' ');
                     lastSection = trimmedSectionName;
                     _iniCollection.Add(lastSection, new Dictionary<string, string>());
-                    counter += 1;
+                    counter++;
                 }
                 else if (commentlessString.Contains("="))
                 {
-                    var spacelessString = _WhtiespaceRemover(commentlessString);
+                    var spacelessString = _WhitespaceRemover(commentlessString);
                     if (spacelessString.Split("=").Length < 1)
                     {
                         string parameter = spacelessString;
-                        _iniCollection[lastSection].Add(parameter, String.Empty);
+                        _iniCollection[lastSection].Add(parameter, string.Empty);
                     }
                     else
                     {
@@ -167,7 +129,7 @@ namespace IniParser
                         _iniCollection[lastSection].Add(parameter, value);
                     }
 
-                    counter += 1;
+                    counter++;
                 }
                 else
                 {
@@ -176,7 +138,6 @@ namespace IniParser
                 }
 
             }
-            // Console.WriteLine($"\nRead total of {counter} lines.");
             return _iniCollection;
         }
 
--- IniParser/Program.cs
+++ IniParser/Program.cs
@@ -8,16 +8,16 @@ namespace IniParser
         static void Main(string[] args)
         {
             IniParser parser = new IniParser();
-            parser.SetFilePath(@"...", true);
+            parser.SetFilePath(@"C:\Users\nelii\OneDrive\Repos\OOPLabs\IniParser\info.ini", true);
             var collection = parser.IniParse();
             parser.PrintIniCollection();
-            parser.TryGetString("NCMD", "Test", out var str);
+            parser.TryGet<string>("NCMD", "Test", out var str);
             Console.WriteLine($"\n{str}");
-            parser.TryGetFloat("ADC_DEV", "BufferLenSecons", out var flt);
-            Console.WriteLine($"\n{flt}");
+            parser.TryGet<double>("ADC_DEV", "BufferLenSecons", out var flt);
+            Console.WriteLine($"{flt}");
+            parser.TryGet<double>("FUN", "HelloFrediKats", out var dbl);
+            Console.WriteLine($"{dbl}");
 
         }
-
-        
     }
 }
